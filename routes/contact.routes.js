51470ed3bef3231a89152572eb7fc591f5216cd5
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-14 13:04:30 +0100

    changing some nodemailer settings. just email password missing

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,38 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
-// const nodemailer = require("nodemailer");
-// require("dotenv").config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const Contact = require("../models/Contact.model");
 
 // NODEMAILER: ACTIVATE LATER
 
-// const transporter = nodemailer.createTransport({
-//     host: "smtp.office365.com", // Outlook SMTP server
-//     port: 587,                 // Secure SMTP port
-//     secure: false,             // Use TLS
-//     auth: {
-//       user: process.env.EMAIL_ADDRESS, // Your Outlook email address
-//       pass: process.env.EMAIL_PASSWORD, // Your Outlook email password
-//     },
-//   });
+const transporter = nodemailer.createTransport({
+    host: "smtp.office365.com", // Outlook SMTP server
+    port: 587,                 // Secure SMTP port
+    secure: false,             // Use TLS
+    auth: {
+      user: process.env.EMAIL_ADDRESS, // Your Outlook email address
+      pass: process.env.EMAIL_PASSWORD, // Your Outlook email password
+    },
+  });
 
-// async function sendEmail(contactData) {
-//   const mailOptions = {
-//     from: contactData.email,
-//     to: process.env.EMAIL_ADDRESS,
-//     subject: `New contact form message from ${contactData.fullName}`,
-//     text: `${contactData.message}`,
-//   };
-//   try {
-//     await transporter.sendMail(mailOptions);
-//     console.log("Message sent successfully to email");
-//   } catch (error) {
-//     console.error("Error sending message to email", error.message);
-//     throw new Error("Failed to send email");
-//   }
-// }
+async function sendEmail(contactData) {
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: `New contact form message from ${contactData.fullName}`,
+    text: `Message: ${contactData.message}. Sender's email: ${contactData.email} . Sender's phone: ${contactData.phoneNumber}`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Message sent successfully to email", mailOptions);
+  } catch (error) {
+    console.error("Error sending message to email", error.message);
+    throw new Error("Failed to send email");
+  }
+}
 
 // CONTACT FORM ROUTE:
 
@@ -48,7 +48,7 @@ router.post("/", async (req, res, next) => {
 
   try {
     const response = await Contact.create(newContact);
-    // await sendEmail(newContact); activate when Nodemailer is functional
+    await sendEmail(newContact); // activate when Nodemailer is functional
 
     res.status(201).json({
       message: "Message received and email sent successfully",
